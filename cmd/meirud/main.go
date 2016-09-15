8665aa4ced9feb1da27eaad7dee3fb2ca02ee1c3
Hamcha <zikyky@gmail.com>
2016-09-15 16:22:19 +0200

    cmd/meirud: Add bolt as database
    Meirud will use Bolt (https://github.com/boltdb/bolt) as database for
    syncing up sendqueues and other persistent informations that are linked to
    MTA tasks.

--- cmd/meirud/main.go
+++ cmd/meirud/main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"log"
 	"os"
+	"time"
 
+	"github.com/boltdb/bolt"
 	"github.com/hamcha/meiru/lib/config"
 )
 
@@ -24,6 +26,20 @@ func main() {
 	var err error
 	conf, err = config.LoadConfig(*cfgpath)
 	assert(err)
+
+	dbpath, err := conf.QuerySingle("dbfile 0")
+	if err == config.QueryErrSingleTooFewResults {
+		log.Fatalln("The configuration value 'dbpath <path/to/db>' is missing! Please add it to the configuration file!")
+	}
+	if err == config.QueryErrSingleTooFewValues {
+		log.Fatalln("The configuration value 'dbpath <path/to/db>' is declared without a value! Please add a path to the database file (will be created if missing).")
+	}
+
+	db, err := bolt.Open(dbpath, 0600, &bolt.Options{Timeout: 10 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
 	if *dump {
 		conf.Dump(os.Stderr)
